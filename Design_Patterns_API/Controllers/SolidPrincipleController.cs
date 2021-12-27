a5d110eac9b2524a0340a6a490c5a3781314e2ef
Ehshanul Hasan <ehshanul.hasan@hi-techbase.com>
2021-12-28 00:42:18 +0600

    project structure created

---
+++ Design_Patterns_API/Controllers/SolidPrincipleController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Design_Patterns_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SolidPrincipleController : ControllerBase
+    {
+        private readonly ILogger<SolidPrincipleController> _logger;
+
+        public SolidPrincipleController(ILogger<SolidPrincipleController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
